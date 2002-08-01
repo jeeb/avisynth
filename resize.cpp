c594a603a3de4d5c6c9c37e7e38822a1401d9720
sh0dan <sh0dan>
2002-08-01 07:28:53 +0000

    MMX optimized HorizontalReduceBy2() in YUY2 mode. Corrected one pixel offset bug in VerticalReduceby2(). MMX is now pixelperfect in compare with C implementations. Changed order in ReduceBy2() since VerticalReduceBy2 is faster.

--- resize.cpp
+++ resize.cpp
@@ -84,6 +84,7 @@ PVideoFrame VerticalReduceBy2::GetFrame(int n, IScriptEnvironment* env) {
 /*************************************
  ******* Vertical 2:1 Reduction ******
  ******* MMX Optimized          ******
+ ******* by Klaus Post          ******
  ************************************/
 
 
@@ -106,13 +107,13 @@ void VerticalReduceBy2::mmx_process(PVideoFrame src,BYTE* dstp, int dst_pitch) {
   }
   if ((row_size&3)==0) {  // row width divideable with 4 (one dword per loop)
     __asm {
-      mov R_XOFFSET,0
+				add [srcp],-4
+        mov R_XOFFSET,0
         mov R_SRC,srcp
         mov R_DST,dstp
         mov R_SRC_PITCH,[src_pitch]
         mov R_DST_PITCH,[dst_pitch]
         mov R_YLEFT,[height]
-        add R_DST,4   //don't ask why
 loopback:
       pxor mm1,mm1
         punpckhbw mm0,[R_SRC]  // line0
@@ -195,11 +196,16 @@ PVideoFrame HorizontalReduceBy2::GetFrame(int n, IScriptEnvironment* env)
 
   const int src_gap = src->GetPitch() - src->GetRowSize();  //aka 'modulo' in VDub filter terminology
   const int dst_gap = dst->GetPitch() - dst->GetRowSize();
+  const int dst_pitch = dst->GetPitch();
 
-  const BYTE* srcp = src->GetReadPtr();
   BYTE* dstp = dst->GetWritePtr();
 
   if (vi.IsYUY2()) {
+    if (env->GetCPUFlags() & CPUF_INTEGER_SSE) {
+			isse_process_yuy2(src,dstp,dst_pitch);
+			return dst;
+		}
+  const BYTE* srcp = src->GetReadPtr();
     for (int y = vi.height; y>0; --y) {
       for (int x = (vi.width>>1)-1; x; --x) {
         dstp[0] = (srcp[0] + 2*srcp[2] + srcp[4] + 2) >> 2;
@@ -215,8 +221,10 @@ PVideoFrame HorizontalReduceBy2::GetFrame(int n, IScriptEnvironment* env)
       dstp[3] = (srcp[3] + srcp[7] + 1) >> 1;
       dstp += dst_gap+4;
       srcp += src_gap+8;
+
     }
   } else if (vi.IsRGB24()) {
+  const BYTE* srcp = src->GetReadPtr();
     for (int y = vi.height; y>0; --y) {
       for (int x = (source_width-1)>>1; x; --x) {
         dstp[0] = (srcp[0] + 2*srcp[3] + srcp[6] + 2) >> 2;
@@ -238,6 +246,7 @@ PVideoFrame HorizontalReduceBy2::GetFrame(int n, IScriptEnvironment* env)
     }
   } else {  //rgb32
     for (int y = vi.height; y>0; --y) {
+  const BYTE* srcp = src->GetReadPtr();
       for (int x = (source_width-1)>>1; x; --x) {
         dstp[0] = (srcp[0] + 2*srcp[4] + srcp[8] + 2) >> 2;
         dstp[1] = (srcp[1] + 2*srcp[5] + srcp[9] + 2) >> 2;
@@ -262,6 +271,152 @@ PVideoFrame HorizontalReduceBy2::GetFrame(int n, IScriptEnvironment* env)
   return dst;
 }
 
+/************************************
+ **** Horizontal 2:1 Reduction ******
+ **** YUY2 Integer SSE Optimized ****
+ **** by Klaus Post              ****
+ ***********************************/
+
+
+#define R_SRC esi
+#define R_DST edi
+#define R_XOFFSET edx
+#define R_YLEFT ebx
+#define R_TEMP1 eax
+#define R_TEMP2 ecx
+
+void HorizontalReduceBy2::isse_process_yuy2(PVideoFrame src,BYTE* dstp, int dst_pitch) {
+  
+  const BYTE* srcp = src->GetReadPtr();
+  const int src_pitch = src->GetPitch();
+  int row_size = ((src->GetRowSize())>>1)-4;
+	
+  const int height = vi.height;
+  __declspec(align(8)) static const __int64 add_2=0x0002000200020002;
+  __declspec(align(8)) static const __int64 zero_mask=0x000000000000ffff;
+  __declspec(align(8)) static const __int64 three_mask=0x0000ffff00000000;
+  __declspec(align(8)) static const __int64 inv_0_mask=0xffffffffffff0000;
+  __declspec(align(8)) static const __int64 inv_3_mask=0xffff0000ffffffff;
+  __asm {
+    movq mm6,[zero_mask];
+    movq mm7,[inv_0_mask];
+  }
+/**
+ * The matrix is flipped, so the mmx registers are equivalent to
+ *  the downward column, and pixels are then added sideways in parallel.
+ * The last two pixels are handled seperately (as in the C-code), 
+ *  with naiive code - but it's only the last pixel.
+ **/
+  if ((row_size&3)==0) {  // row width divideable with 8 (one qword per loop)
+    __asm {
+      mov R_XOFFSET,0
+				add [srcp],-4
+				prefetchnta [srcp]
+				prefetchnta [srcp+64]
+        mov R_SRC,srcp
+        mov R_DST,dstp
+        mov R_YLEFT,[height]
+loop_nextline:
+				mov R_TEMP2,[row_size]
+loopback:
+				prefetchnta [R_SRC+64]
+        punpckhbw mm0,[R_SRC]  // pixel 1
+        punpckhbw mm1,[R_SRC+4]  // pixel 2
+        punpckhbw mm2,[R_SRC+8]  // pixel 3
+				psrlw mm0,8				;mm0=00VV 00Y2 00UU 00Y1
+				psrlw mm1,8				;mm1=00VV 00Y2 00UU 00Y1
+				psrlw mm2,8				;mm2=00VV 00Y2 00UU 00Y1
+				movq mm3,mm0								; word 3 [4] word 0 in mm1 (ok)
+				movq mm4,mm1								; word 0 [2] must be exchanged with word 2 in mm0
+				pand mm3,[inv_3_mask]
+				pshufw mm5,mm2,11000100b		; word 0 [4] must be exhanged (word0 in mm1) (ok)
+				pand mm4,mm7
+				pshufw mm1,mm1,0
+				pand mm5,mm7
+				movq mm2,mm1
+        pand mm1,mm6
+				pshufw mm0,mm0,10101010b
+        pand mm2,[three_mask]
+        pand mm0,mm6
+				por mm4,mm0
+				por mm3,mm2
+				psllw mm4,1
+				por mm5,mm1
+				paddw mm3,mm4
+				paddw mm5,[add_2]
+				paddw mm3,mm5
+        psrlw mm3,2
+        packuswb mm3,mm1
+        movd [R_DST+R_XOFFSET],mm3
+
+				add R_SRC,8
+        add R_XOFFSET,4
+        cmp  R_XOFFSET,R_TEMP2
+        jl loopback						; Jump back
+				// Last two pixels
+				// Could be optimized greatly, but it only runs once per line
+
+			mov R_TEMP1,[R_SRC]
+			mov R_TEMP2,[R_SRC+2]
+			and R_TEMP1,255
+			and R_TEMP2,255
+			add R_TEMP1,R_TEMP2
+			add R_TEMP1,R_TEMP2
+			mov R_TEMP2,[R_SRC+4]
+			add R_TEMP1,2
+			and R_TEMP2,255
+			add R_TEMP1,R_TEMP2
+			shr R_TEMP1,2
+			mov [R_DST+R_XOFFSET],R_TEMP1
+
+			mov R_TEMP1,[R_SRC+1]
+			mov R_TEMP2,[R_SRC+5]
+			and R_TEMP1,255
+			and R_TEMP2,255
+			add R_TEMP1,1
+			add R_TEMP1,R_TEMP2
+			shr R_TEMP1,1
+			shl R_TEMP1,8
+			or [R_DST+R_XOFFSET],R_TEMP1
+
+			mov R_TEMP1,[R_SRC+4]
+			mov R_TEMP2,[R_SRC+6]
+			and R_TEMP1,255
+			and R_TEMP2,255
+			add R_TEMP1,1
+			add R_TEMP1,R_TEMP2
+			shr R_TEMP1,1
+			shl R_TEMP1,16
+			or [R_DST+R_XOFFSET],R_TEMP1
+
+			mov R_TEMP1,[R_SRC+3]
+			mov R_TEMP2,[R_SRC+7]
+			and R_TEMP1,255
+			and R_TEMP2,255
+			add R_TEMP1,1
+			add R_TEMP1,R_TEMP2
+			shr R_TEMP1,1
+			shl R_TEMP1,24
+			or [R_DST+R_XOFFSET],R_TEMP1
+
+				mov R_TEMP2,[src_pitch]
+        add srcp, R_TEMP2
+        mov R_XOFFSET,0
+        add R_DST,[dst_pitch]
+        mov R_SRC,srcp
+        dec R_YLEFT
+        jnz loop_nextline        
+        emms
+    }
+  }
+}
+
+#undef R_SRC
+#undef R_DST
+#undef R_XOFFSET
+#undef R_YLEFT
+#undef R_SRC_PITCH
+#undef R_DST_PITCH
 
 
 
@@ -273,7 +428,7 @@ PVideoFrame HorizontalReduceBy2::GetFrame(int n, IScriptEnvironment* env)
 
 AVSValue __cdecl Create_ReduceBy2(AVSValue args, void*, IScriptEnvironment* env) 
 {
-  return new VerticalReduceBy2(new HorizontalReduceBy2(args[0].AsClip(), env),env);
+  return new HorizontalReduceBy2(new VerticalReduceBy2(args[0].AsClip(), env),env);
 }
 
 
