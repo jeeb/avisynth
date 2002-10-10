db54d5afe46110f13db67dd3454320a4b6ef17df
sh0dan <sh0dan>
2002-10-10 07:34:57 +0000

    Added bool to manually disable seeking in DirectshowSource. Good for WMV's, that doesn't return an error, but just is slow as h***.

--- source.cpp
+++ source.cpp
@@ -1221,9 +1221,9 @@ class DirectShowSource : public IClip {
 
 public:
 
-  DirectShowSource(const char* filename, int _avg_time_per_frame, IScriptEnvironment* _env) : env(_env), get_sample(_env) {
+  DirectShowSource(const char* filename, int _avg_time_per_frame, bool _seek,IScriptEnvironment* _env) : env(_env), get_sample(_env) {
     CheckHresult(CoCreateInstance(CLSID_FilterGraphNoThread, 0, CLSCTX_INPROC_SERVER, IID_IGraphBuilder, (void**)&gb), "couldn't create filter graph");
-
+    no_search=!_seek;
     WCHAR filenameW[MAX_PATH];
     MultiByteToWideChar(CP_ACP, 0, filename, -1, filenameW, MAX_PATH);
     CheckHresult(gb->AddFilter(static_cast<IBaseFilter*>(&get_sample), L"GetSample"), "couldn't add GetSample filter");
@@ -1274,7 +1274,6 @@ public:
 
     cur_frame = 0;
     base_sample_time = 0;
-    no_search = false;
   }
 
   ~DirectShowSource() { get_sample.StopGraph(); gb->Release(); }
@@ -1305,7 +1304,8 @@ public:
     } else {
       __int64 sample_time = __int64(n) * avg_time_per_frame + (avg_time_per_frame>>1);
       if (n > cur_frame || n > cur_frame+10) {
-        if (get_sample.SeekTo(sample_time)!=S_OK) {
+        if (no_search || get_sample.SeekTo(sample_time)!=S_OK) {
+          no_search=true;
           if (cur_frame<n) {  // seek manually
             while (get_sample.GetSampleEndTime()+base_sample_time <= sample_time) {
               get_sample.NextSample();
@@ -1350,7 +1350,7 @@ void DirectShowSource::CheckHresult(HRESULT hr, const char* msg, const char* msg
 AVSValue __cdecl Create_DirectShowSource(AVSValue args, void*, IScriptEnvironment* env) {
   const char* filename = args[0][0].AsString();
   int avg_time_per_frame = args[1].Defined() ? int(10000000 / args[1].AsFloat() + 0.5) : 0;
-  return new DirectShowSource(filename, avg_time_per_frame, env);
+  return new DirectShowSource(filename, avg_time_per_frame, args[2].AsBool(true), env);
 }
 
 
@@ -1399,7 +1399,7 @@ AVSValue __cdecl Create_SegmentedSource(AVSValue args, void* use_directshow, ISc
       wsprintf(filename, "%s.%02d.%s", basename, j, extension);
       if (GetFileAttributes(filename) != (DWORD)-1) {   // check if file exists
         try {
-          PClip clip = use_directshow ? (IClip*)(new DirectShowSource(filename, avg_time_per_frame, env))
+          PClip clip = use_directshow ? (IClip*)(new DirectShowSource(filename, avg_time_per_frame, args[2].AsBool(true),env))
                                     : (IClip*)(new AVISource(filename, bAudio, pixel_type, 0, env));
           result = !result ? clip : new_Splice(result, clip, false, env);
         } catch (AvisynthError e) {
@@ -1409,7 +1409,7 @@ AVSValue __cdecl Create_SegmentedSource(AVSValue args, void* use_directshow, ISc
     }
   }
   if (!result) {
-    if (!error_msg) {
+    if (!error_msg) { 
       env->ThrowError("Segmented%sSource: no files found!", use_directshow ? "DirectShow" : "AVI");
     } else {
       env->ThrowError("Segmented%sSource: decompressor returned error:\n%s!", use_directshow ? "DirectShow" : "AVI",error_msg);
@@ -1431,9 +1431,9 @@ AVSFunction Source_filters[] = {
   { "AVIFileSource", "s+[audio]b[pixel_type]s", AVISource::Create, (void*)1 },
   { "WAVSource", "s+", AVISource::Create, (void*)3 },
   { "OpenDMLSource", "s+[audio]b[pixel_type]s", AVISource::Create, (void*)2 },
-  { "DirectShowSource", "s+[fps]f", Create_DirectShowSource },
+  { "DirectShowSource", "s+[fps]f[seek]b", Create_DirectShowSource },
   { "SegmentedAVISource", "s+[audio]b[pixel_type]s", Create_SegmentedSource, (void*)0 },
-  { "SegmentedDirectShowSource", "s+[fps]f", Create_SegmentedSource, (void*)1 },
+  { "SegmentedDirectShowSource", "s+[fps]f[seek]b", Create_SegmentedSource, (void*)1 },
 //  { "QuickTimeSource", "s", QuickTimeSource::Create },
   { "BlankClip", "[clip]c[length]i[width]i[height]i[pixel_type]s[fps]f[fps_denominator]i[audio_rate]i[stereo]b[sixteen_bit]b[color]i", Create_BlankClip },
   { "Blackness", "[clip]c[length]i[width]i[height]i[pixel_type]s[fps]f[fps_denominator]i[audio_rate]i[stereo]b[sixteen_bit]b[color]i", Create_BlankClip },
