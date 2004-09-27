15aeb1aef8e257a8855b8ce9a49c6021596e3be7
ianb1957 <ianb1957>
2004-09-27 00:55:42 +0000

    Version 2.56

--- src/internal.h
+++ src/internal.h
@@ -36,8 +36,8 @@
 #ifndef __Internal_H__
 #define __Internal_H__
 
-#define AVS_VERSION 2.55
-#define AVS_VERSTR "AviSynth 2.55, build:"__DATE__" ["__TIME__"]"
+#define AVS_VERSION 2.56
+#define AVS_VERSTR "AviSynth 2.56, build:"__DATE__" ["__TIME__"]"
 
 
 #include "core/avisynth.h"
