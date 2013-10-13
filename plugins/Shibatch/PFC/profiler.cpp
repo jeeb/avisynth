a513965aec6665f3b3736cc6ee2084700ae3ec55
pylorak <software@pados.hu>
2013-10-13 15:52:50 +0200

    [64bit] Port Shibatch to 64-bit.

--- plugins/Shibatch/PFC/profiler.cpp
+++ plugins/Shibatch/PFC/profiler.cpp
@@ -1,11 +1,7 @@
 #include "pfc.h"
+#include <intrin.h>
 
-
-__declspec(naked) __int64 profiler_local::get_timestamp()
+__int64 profiler_local::get_timestamp()
 {
-	__asm
-	{
-		rdtsc
-		ret
-	}
+  return __rdtsc();
 }
