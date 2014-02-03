9f7ee21b67487c39cd0caf9e0deda488f3b200d4
innocenat <innocenat@server.fake>
2014-02-03 20:00:36 +0700

    Color: register as NICE_FILTER

--- avs_core/filters/color.h
+++ avs_core/filters/color.h
@@ -73,6 +73,11 @@ public:
 
     PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment* env);
 
+    int __stdcall SetCacheHints(int cachehints, int frame_range) override
+    {
+        return cachehints == CACHE_GET_MTMODE ? MT_NICE_FILTER : 0;
+    }
+
     static AVSValue __cdecl Create(AVSValue args, void*, IScriptEnvironment* env);
 
 private:
