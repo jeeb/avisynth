a39a84b4afe90539709cd4f4d6e9ff86a840177b
pylorak <software@pados.hu>
2014-03-01 15:32:02 +0100

    Register Splice as a NICE filter.

--- avs_core/filters/edit.cpp
+++ avs_core/filters/edit.cpp
@@ -509,6 +509,8 @@ int Splice::SetCacheHints(int cachehints,int frame_range)
   {
   case CACHE_DONT_CACHE_ME:
     return 1;
+  case CACHE_GET_MTMODE:
+    return MT_NICE_FILTER;
   default:
     if (passCache) {
       child2->SetCacheHints(cachehints, frame_range);
