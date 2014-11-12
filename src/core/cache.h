aada5d52c2e0a8c98e8df062964fbea514ed92ff
ianb1957 <ianb1957>
2014-11-12 05:55:01 +0000

    Remove legacy 2.5 cache definitions from Avisynth.h

--- src/core/cache.h
+++ src/core/cache.h
@@ -155,6 +155,17 @@ private:
 //  unsigned int prefetch_audio_starthi;
 //  unsigned int prefetch_audio_count;
 
+  enum {
+    // Old 2.5 poorly defined cache hints.
+    // Reserve values used by 2.5 API
+    // Do not use in new filters
+    CACHE_25_NOTHING=0, 
+    CACHE_25_RANGE=1,
+    CACHE_25_ALL=2,
+    CACHE_25_AUDIO=3,
+    CACHE_25_AUDIO_NONE=4,
+    CACHE_25_AUDIO_AUTO=5,
+  };
 };
 
 #endif  // __Cache_H__
