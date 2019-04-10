e2f4103cee1f099541640226641d3dc9909e6b82
Pinterf <pinterfer@gmail.com>
2019-04-10 14:32:55 +0200

    Soundtouch: no header for gcc neither

--- plugins/TimeStretch/SoundTouch/STTypes.h
+++ plugins/TimeStretch/SoundTouch/STTypes.h
@@ -50,7 +50,9 @@ typedef unsigned long   ulong;
 #if (defined(__GNUC__) && !defined(ANDROID))
     // In GCC, include soundtouch_config.h made by config scritps.
     // Skip this in Android compilation that uses GCC but without configure scripts.
-    #include "soundtouch_config.h"
+
+    // FIXME: check it what is does, because it compiles nicely without this line
+    // #include "soundtouch_config.h"
 #endif
 
 
