61f163b2fdc527997525d0e9ab094170b4a3ad87
Enyium <123484196+Enyium@users.noreply.github.com>
2023-02-02 12:16:57 +0100

    Improved enum code layout.

--- avs_core/include/avisynth.h
+++ avs_core/include/avisynth.h
@@ -140,29 +140,31 @@ enum {
 // information that does not depend on the frame number).  The GetVideoInfo
 // method in IClip returns this struct.
 
-enum {SAMPLE_INT8  = 1<<0,
-      SAMPLE_INT16 = 1<<1,
-      SAMPLE_INT24 = 1<<2,    // Int24 is a very stupid thing to code, but it's supported by some hardware.
-      SAMPLE_INT32 = 1<<3,
-      SAMPLE_FLOAT = 1<<4};
+enum {
+  SAMPLE_INT8  = 1 << 0,
+  SAMPLE_INT16 = 1 << 1,
+  SAMPLE_INT24 = 1 << 2,  // Int24 is a very stupid thing to code, but it's supported by some hardware.
+  SAMPLE_INT32 = 1 << 3,
+  SAMPLE_FLOAT = 1 << 4
+};
 
 enum {
-   PLANAR_Y=1<<0,
-   PLANAR_U=1<<1,
-   PLANAR_V=1<<2,
-   PLANAR_ALIGNED=1<<3,
-   PLANAR_Y_ALIGNED=PLANAR_Y|PLANAR_ALIGNED,
-   PLANAR_U_ALIGNED=PLANAR_U|PLANAR_ALIGNED,
-   PLANAR_V_ALIGNED=PLANAR_V|PLANAR_ALIGNED,
-   PLANAR_A=1<<4,
-   PLANAR_R=1<<5,
-   PLANAR_G=1<<6,
-   PLANAR_B=1<<7,
-   PLANAR_A_ALIGNED=PLANAR_A|PLANAR_ALIGNED,
-   PLANAR_R_ALIGNED=PLANAR_R|PLANAR_ALIGNED,
-   PLANAR_G_ALIGNED=PLANAR_G|PLANAR_ALIGNED,
-   PLANAR_B_ALIGNED=PLANAR_B|PLANAR_ALIGNED,
-  };
+  PLANAR_Y = 1 << 0,
+  PLANAR_U = 1 << 1,
+  PLANAR_V = 1 << 2,
+  PLANAR_ALIGNED = 1 << 3,
+  PLANAR_Y_ALIGNED = PLANAR_Y | PLANAR_ALIGNED,
+  PLANAR_U_ALIGNED = PLANAR_U | PLANAR_ALIGNED,
+  PLANAR_V_ALIGNED = PLANAR_V | PLANAR_ALIGNED,
+  PLANAR_A = 1 << 4,
+  PLANAR_R = 1 << 5,
+  PLANAR_G = 1 << 6,
+  PLANAR_B = 1 << 7,
+  PLANAR_A_ALIGNED = PLANAR_A | PLANAR_ALIGNED,
+  PLANAR_R_ALIGNED = PLANAR_R | PLANAR_ALIGNED,
+  PLANAR_G_ALIGNED = PLANAR_G | PLANAR_ALIGNED,
+  PLANAR_B_ALIGNED = PLANAR_B | PLANAR_ALIGNED,
+};
 
 class AvisynthError /* exception */ {
 public:
@@ -496,95 +498,93 @@ public:
 #endif
 };
 struct VideoInfo {
-  int width, height;    // width=0 means no video
+  int width, height;    // width 0 means no video
   unsigned fps_numerator, fps_denominator;
   int num_frames;
   // This is more extensible than previous versions. More properties can be added seeminglesly.
 
   // Colorspace properties.
-/*
-
-Planar match mask  1111.1000.0000.0111.0000.0111.0000.0111
-Planar signature   10xx.1000.0000.00xx.0000.00xx.00xx.00xx ?
-Planar signature   10xx.1000.0000.0xxx.0000.00xx.000x.x0xx ? *new
-Planar filter mask 1111.1111.1111.1111.1111.1111.1110.0111 (typo from old header fixed)
-
-pixel_type mapping
-==================
-pixel_type bit-map PIYB.Z000.0???.0SSS.0000.0???.????.????
-        planar YUV            CCC            HHH.000u.vWWW
-     planar RGB(A)            CCC                       AR
-         nonplanar            CCC            000.00wx xyAR
-Legend
-======
-Planar YUV:
-  Code Bits Remark
-  W    0-2  Planar Width Subsampling bits
-            Use (X+1) & 3 for GetPlaneWidthSubsampling
-              000 => 1        YV12, YV16, YUV420, YUV422
-              001 => 2        YV411, YUV9
-              010 => reserved
-              011 => 0        YV24, YUV444, RGBP
-              1xx => reserved
-  v    3    VPlaneFirst YV12, YV16, YV24, YV411, YUV9
-  u    4    UPlaneFirst I420
-  H    7-9  Planar Height Subsampling bits
-            Use ((X>>8)+1) & 3 for GetPlaneHeightSubsampling
-              000 => 1        YV12, YUV420
-              001 => 2        YUV9
-              010 => reserved
-              011 => 0        YV16, YV24, YV411, YUV422, YUV444, RGBP
-              1xx => reserved
-
-Planar RGB
- Code Bits Remark
-   R   0   BGR,  (with SSS bits for 8/16 bit/sample or float)
-   A   1   BGRA, (with SSS bits for 8/16 bit/sample or float)
-
-
-Not Planar, Interleaved (I flag)
-Code Bits Remark
-  R   0   BGR24, and BGRx in future (with SSS bits for 8/16 bit/sample or float)
-  A   1   BGR32, and BGRAx in future (with SSS bits for 8/16 bit/sample or float)
-  y   2   YUY2
-  x   3-4 reserved
-  w   5   Raw32
-
-General
-Code Bits Remark
-  S  16-18 Sample resolution bits
-          000 => 8
-          001 => 16
-          010 => 32 (float)
-          011,100 => reserved
-          101 => 10 bits
-          110 => 12 bits
-          111 => 14 bits
-for packed RGB(A): only 8 and 16 bits are valid
-
-Other YV12 specific (will never be used)
-  C  20-23 Chroma Placement values 0-4; see CS_xxx_CHROMA_PLACEMENT
-
-Color family and layout
-                       Packed      Planar               Planar  Planar
-Code Bits Remark       RGB/RGBA     YUV  YUY2  Y_Grey  RGB/RGBA  YUVA
-  R   0                  1/0         -    0      -       1/0       -
-  A   1                  0/1         -    0      -       0/1       -
-  y   2                   -          -    1      -        0        -
-  Z  27   YUVA            0          0    0      0        1        1
-  B  28   BGR             1          0    0      0        1*       0
-  Y  29   YUV             0          1    1      1        0        0
-  I  30   Interleaved     1          0    1      1        0        0
-  P  31   Planar          0          1    0      1        1        1
-* Planar RGB plane order: G,B,R(,A)
-
-*/
-enum {
-    CS_YUVA = 1<<27,
-    CS_BGR = 1<<28,
-    CS_YUV = 1<<29,
-    CS_INTERLEAVED = 1<<30,
-    CS_PLANAR = 1<<31,
+  /*
+    Planar match mask  1111.1000.0000.0111.0000.0111.0000.0111
+    Planar signature   10xx.1000.0000.00xx.0000.00xx.00xx.00xx ?
+    Planar signature   10xx.1000.0000.0xxx.0000.00xx.000x.x0xx ? *new
+    Planar filter mask 1111.1111.1111.1111.1111.1111.1110.0111 (typo from old header fixed)
+    
+    pixel_type mapping
+    ==================
+    pixel_type bit-map PIYB.Z000.0???.0SSS.0000.0???.????.????
+            planar YUV            CCC            HHH.000u.vWWW
+         planar RGB(A)            CCC                       AR
+             nonplanar            CCC            000.00wx xyAR
+    Legend
+    ======
+    Planar YUV:
+      Code Bits Remark
+      W    0-2  Planar Width Subsampling bits
+                Use (X+1) & 3 for GetPlaneWidthSubsampling
+                  000 => 1        YV12, YV16, YUV420, YUV422
+                  001 => 2        YV411, YUV9
+                  010 => reserved
+                  011 => 0        YV24, YUV444, RGBP
+                  1xx => reserved
+      v    3    VPlaneFirst YV12, YV16, YV24, YV411, YUV9
+      u    4    UPlaneFirst I420
+      H    7-9  Planar Height Subsampling bits
+                Use ((X>>8)+1) & 3 for GetPlaneHeightSubsampling
+                  000 => 1        YV12, YUV420
+                  001 => 2        YUV9
+                  010 => reserved
+                  011 => 0        YV16, YV24, YV411, YUV422, YUV444, RGBP
+                  1xx => reserved
+    
+    Planar RGB
+     Code Bits Remark
+       R   0   BGR,  (with SSS bits for 8/16 bit/sample or float)
+       A   1   BGRA, (with SSS bits for 8/16 bit/sample or float)
+    
+    
+    Not Planar, Interleaved (I flag)
+    Code Bits Remark
+      R   0   BGR24, and BGRx in future (with SSS bits for 8/16 bit/sample or float)
+      A   1   BGR32, and BGRAx in future (with SSS bits for 8/16 bit/sample or float)
+      y   2   YUY2
+      x   3-4 reserved
+      w   5   Raw32
+    
+    General
+    Code Bits Remark
+      S 16-18 Sample resolution bits
+              000 => 8
+              001 => 16
+              010 => 32 (float)
+              011,100 => reserved
+              101 => 10 bits
+              110 => 12 bits
+              111 => 14 bits
+    for packed RGB(A): only 8 and 16 bits are valid
+    
+    Other YV12 specific (will never be used)
+      C  20-23 Chroma Placement values 0-4; see CS_xxx_CHROMA_PLACEMENT
+    
+    Color family and layout
+                           Packed      Planar               Planar  Planar
+    Code Bits Remark       RGB/RGBA     YUV  YUY2  Y_Grey  RGB/RGBA  YUVA
+      R   0                  1/0         -    0      -       1/0       -
+      A   1                  0/1         -    0      -       0/1       -
+      y   2                   -          -    1      -        0        -
+      Z  27   YUVA            0          0    0      0        1        1
+      B  28   BGR             1          0    0      0        1*       0
+      Y  29   YUV             0          1    1      1        0        0
+      I  30   Interleaved     1          0    1      1        0        0
+      P  31   Planar          0          1    0      1        1        1
+    * Planar RGB plane order: G,B,R(,A)
+  */
+  enum {
+    CS_YUVA        = 1 << 27,
+    CS_BGR         = 1 << 28,
+    CS_YUV         = 1 << 29,
+    CS_INTERLEAVED = 1 << 30,
+    CS_PLANAR      = 1 << 31,
 
     CS_Shift_Sub_Width   =  0,
     CS_Shift_Sub_Height  =  8,
@@ -611,9 +611,9 @@ enum {
     CS_Sample_Bits_16    = 1 << CS_Shift_Sample_Bits,
     CS_Sample_Bits_32    = 2 << CS_Shift_Sample_Bits,
 
-    CS_PLANAR_MASK       = CS_PLANAR | CS_INTERLEAVED | CS_YUV | CS_BGR | CS_YUVA | CS_Sample_Bits_Mask
-    | CS_Sub_Width_Mask | CS_Sub_Height_Mask,
-    CS_PLANAR_FILTER     = ~( CS_VPlaneFirst | CS_UPlaneFirst ),
+    CS_PLANAR_MASK       = CS_PLANAR | CS_INTERLEAVED | CS_YUV | CS_BGR | CS_YUVA
+                           | CS_Sample_Bits_Mask | CS_Sub_Width_Mask | CS_Sub_Height_Mask,
+    CS_PLANAR_FILTER     = ~(CS_VPlaneFirst | CS_UPlaneFirst),
 
     CS_RGB_TYPE  = 1 << 0,
     CS_RGBA_TYPE = 1 << 1,
@@ -623,10 +623,10 @@ enum {
 
     CS_BGR24 = CS_RGB_TYPE  | CS_BGR | CS_INTERLEAVED,
     CS_BGR32 = CS_RGBA_TYPE | CS_BGR | CS_INTERLEAVED,
-    CS_YUY2  = 1<<2 | CS_YUV | CS_INTERLEAVED,
-    //  CS_YV12  = 1<<3  Reserved
-    //  CS_I420  = 1<<4  Reserved
-    CS_RAW32 = 1<<5 | CS_INTERLEAVED,
+    CS_YUY2  = 1 << 2 | CS_YUV | CS_INTERLEAVED,
+    //  CS_YV12  = 1 << 3  Reserved
+    //  CS_I420  = 1 << 4  Reserved
+    CS_RAW32 = 1 << 5 | CS_INTERLEAVED,
 
     //  YV12 must be 0xA0000008. v2.5 Baked API will see all new planar as YV12.
     //  I420 must be 0xA0000010.
@@ -728,25 +728,22 @@ enum {
     CS_YUVA444PS  = CS_GENERIC_YUVA444 | CS_Sample_Bits_32,  // YUVA 4:4:4 32bit samples
     CS_YUVA422PS  = CS_GENERIC_YUVA422 | CS_Sample_Bits_32,  // YUVA 4:2:2 32bit samples
     CS_YUVA420PS  = CS_GENERIC_YUVA420 | CS_Sample_Bits_32,  // YUVA 4:2:0 32bit samples
-
   };
 
   int pixel_type;                // changed to int as of 2.5
-
 
   int audio_samples_per_second;   // 0 means no audio
   int sample_type;                // as of 2.5
   int64_t num_audio_samples;      // changed as of 2.5
   int nchannels;                  // as of 2.5
 
-  // Imagetype properties
-
   int image_type;
 
+  // Image type bit flags
   enum {
-    IT_BFF = 1<<0,
-    IT_TFF = 1<<1,
-    IT_FIELDBASED = 1<<2
+    IT_BFF        = 1 << 0,
+    IT_TFF        = 1 << 1,
+    IT_FIELDBASED = 1 << 2
   };
 
   // Chroma placement bits 20 -> 23  ::FIXME:: Really want a Class to support this
@@ -864,7 +861,6 @@ enum {
 
   // Planar RGBA?
   bool IsPlanarRGBA() const AVS_BakedCode( return AVS_LinkCallOptDefault(IsPlanarRGBA, false) )
-
 }; // end struct VideoInfo
 
 
@@ -1030,55 +1026,55 @@ enum CachePolicyHint {
   // do not use them in new plugins
 
   // New 2.6 explicitly defined cache hints.
-  CACHE_NOTHING=10, // Do not cache video.
-  CACHE_WINDOW=11, // Hard protect upto X frames within a range of X from the current frame N.
-  CACHE_GENERIC=12, // LRU cache upto X frames.
-  CACHE_FORCE_GENERIC=13, // LRU cache upto X frames, override any previous CACHE_WINDOW.
+  CACHE_NOTHING = 10, // Do not cache video.
+  CACHE_WINDOW = 11, // Hard protect upto X frames within a range of X from the current frame N.
+  CACHE_GENERIC = 12, // LRU cache upto X frames.
+  CACHE_FORCE_GENERIC = 13, // LRU cache upto X frames, override any previous CACHE_WINDOW.
 
-  CACHE_GET_POLICY=30, // Get the current policy.
-  CACHE_GET_WINDOW=31, // Get the current window h_span.
-  CACHE_GET_RANGE=32, // Get the current generic frame range.
+  CACHE_GET_POLICY = 30, // Get the current policy.
+  CACHE_GET_WINDOW = 31, // Get the current window h_span.
+  CACHE_GET_RANGE = 32, // Get the current generic frame range.
 
-  CACHE_AUDIO=50, // Explicitly cache audio, X byte cache.
-  CACHE_AUDIO_NOTHING=51, // Explicitly do not cache audio.
-  CACHE_AUDIO_NONE=52, // Audio cache off (auto mode), X byte intial cache.
-  CACHE_AUDIO_AUTO=53, // Audio cache on (auto mode), X byte intial cache.
+  CACHE_AUDIO = 50, // Explicitly cache audio, X byte cache.
+  CACHE_AUDIO_NOTHING = 51, // Explicitly do not cache audio.
+  CACHE_AUDIO_NONE = 52, // Audio cache off (auto mode), X byte intial cache.
+  CACHE_AUDIO_AUTO = 53, // Audio cache on (auto mode), X byte intial cache.
 
-  CACHE_GET_AUDIO_POLICY=70, // Get the current audio policy.
-  CACHE_GET_AUDIO_SIZE=71, // Get the current audio cache size.
+  CACHE_GET_AUDIO_POLICY = 70, // Get the current audio policy.
+  CACHE_GET_AUDIO_SIZE = 71, // Get the current audio cache size.
 
-  CACHE_PREFETCH_FRAME=100, // Queue request to prefetch frame N.
-  CACHE_PREFETCH_GO=101, // Action video prefetches.
+  CACHE_PREFETCH_FRAME = 100, // Queue request to prefetch frame N.
+  CACHE_PREFETCH_GO = 101, // Action video prefetches.
 
-  CACHE_PREFETCH_AUDIO_BEGIN=120, // Begin queue request transaction to prefetch audio (take critical section).
-  CACHE_PREFETCH_AUDIO_STARTLO=121, // Set low 32 bits of start.
-  CACHE_PREFETCH_AUDIO_STARTHI=122, // Set high 32 bits of start.
-  CACHE_PREFETCH_AUDIO_COUNT=123, // Set low 32 bits of length.
-  CACHE_PREFETCH_AUDIO_COMMIT=124, // Enqueue request transaction to prefetch audio (release critical section).
-  CACHE_PREFETCH_AUDIO_GO=125, // Action audio prefetches.
+  CACHE_PREFETCH_AUDIO_BEGIN = 120, // Begin queue request transaction to prefetch audio (take critical section).
+  CACHE_PREFETCH_AUDIO_STARTLO = 121, // Set low 32 bits of start.
+  CACHE_PREFETCH_AUDIO_STARTHI = 122, // Set high 32 bits of start.
+  CACHE_PREFETCH_AUDIO_COUNT = 123, // Set low 32 bits of length.
+  CACHE_PREFETCH_AUDIO_COMMIT = 124, // Enqueue request transaction to prefetch audio (release critical section).
+  CACHE_PREFETCH_AUDIO_GO = 125, // Action audio prefetches.
 
-  CACHE_GETCHILD_CACHE_MODE=200, // Cache ask Child for desired video cache mode.
-  CACHE_GETCHILD_CACHE_SIZE=201, // Cache ask Child for desired video cache size.
-  CACHE_GETCHILD_AUDIO_MODE=202, // Cache ask Child for desired audio cache mode.
-  CACHE_GETCHILD_AUDIO_SIZE=203, // Cache ask Child for desired audio cache size.
+  CACHE_GETCHILD_CACHE_MODE = 200, // Cache ask Child for desired video cache mode.
+  CACHE_GETCHILD_CACHE_SIZE = 201, // Cache ask Child for desired video cache size.
+  CACHE_GETCHILD_AUDIO_MODE = 202, // Cache ask Child for desired audio cache mode.
+  CACHE_GETCHILD_AUDIO_SIZE = 203, // Cache ask Child for desired audio cache size.
 
-  CACHE_GETCHILD_COST=220, // Cache ask Child for estimated processing cost.
-    CACHE_COST_ZERO=221, // Child response of zero cost (ptr arithmetic only).
-    CACHE_COST_UNIT=222, // Child response of unit cost (less than or equal 1 full frame blit).
-    CACHE_COST_LOW=223, // Child response of light cost. (Fast)
-    CACHE_COST_MED=224, // Child response of medium cost. (Real time)
-    CACHE_COST_HI=225, // Child response of heavy cost. (Slow)
+  CACHE_GETCHILD_COST = 220, // Cache ask Child for estimated processing cost.
+    CACHE_COST_ZERO = 221, // Child response of zero cost (ptr arithmetic only).
+    CACHE_COST_UNIT = 222, // Child response of unit cost (less than or equal 1 full frame blit).
+    CACHE_COST_LOW = 223, // Child response of light cost. (Fast)
+    CACHE_COST_MED = 224, // Child response of medium cost. (Real time)
+    CACHE_COST_HI = 225, // Child response of heavy cost. (Slow)
 
-  CACHE_GETCHILD_THREAD_MODE=240, // Cache ask Child for thread safetyness.
-    CACHE_THREAD_UNSAFE=241, // Only 1 thread allowed for all instances. 2.5 filters default!
-    CACHE_THREAD_CLASS=242, // Only 1 thread allowed for each instance. 2.6 filters default!
-    CACHE_THREAD_SAFE=243, //  Allow all threads in any instance.
-    CACHE_THREAD_OWN=244, // Safe but limit to 1 thread, internally threaded.
+  CACHE_GETCHILD_THREAD_MODE = 240, // Cache ask Child for thread safetyness.
+    CACHE_THREAD_UNSAFE = 241, // Only 1 thread allowed for all instances. 2.5 filters default!
+    CACHE_THREAD_CLASS = 242, // Only 1 thread allowed for each instance. 2.6 filters default!
+    CACHE_THREAD_SAFE = 243, //  Allow all threads in any instance.
+    CACHE_THREAD_OWN = 244, // Safe but limit to 1 thread, internally threaded.
 
-  CACHE_GETCHILD_ACCESS_COST=260, // Cache ask Child for preferred access pattern.
-    CACHE_ACCESS_RAND=261, // Filter is access order agnostic.
-    CACHE_ACCESS_SEQ0=262, // Filter prefers sequential access (low cost)
-    CACHE_ACCESS_SEQ1=263, // Filter needs sequential access (high cost)
+  CACHE_GETCHILD_ACCESS_COST = 260, // Cache ask Child for preferred access pattern.
+    CACHE_ACCESS_RAND = 261, // Filter is access order agnostic.
+    CACHE_ACCESS_SEQ0 = 262, // Filter prefers sequential access (low cost)
+    CACHE_ACCESS_SEQ1 = 263, // Filter needs sequential access (high cost)
 
   CACHE_AVSPLUS_CONSTANTS = 500,    // Smaller values are reserved for classic Avisynth
 
@@ -1103,7 +1099,6 @@ enum CachePolicyHint {
   CACHE_GET_CHILD_DEV_TYPE,    // Device types a fitler can receive
 
   CACHE_USER_CONSTANTS = 1000       // Smaller values are reserved for the core
-
 };
 
 // Base class for all filters.
@@ -1360,8 +1355,7 @@ public:
 #include "avs/cpuid.h"
 
 // IScriptEnvironment GetEnvProperty
-enum AvsEnvProperty
-{
+enum AvsEnvProperty {
   AEP_PHYSICAL_CPUS = 1,
   AEP_LOGICAL_CPUS = 2,
   AEP_THREADPOOL_THREADS = 3,
@@ -1382,8 +1376,7 @@ enum AvsEnvProperty
 };
 
 // IScriptEnvironment Allocate
-enum AvsAllocType
-{
+enum AvsAllocType {
   AVS_NORMAL_ALLOC = 1,
   AVS_POOLED_ALLOC = 2
 };
@@ -1443,9 +1436,10 @@ public:
   virtual void* __stdcall ManageCache(int key, void* data) = 0;
 
   enum PlanarChromaAlignmentMode {
-            PlanarChromaAlignmentOff,
-            PlanarChromaAlignmentOn,
-            PlanarChromaAlignmentTest };
+    PlanarChromaAlignmentOff,
+    PlanarChromaAlignmentOn,
+    PlanarChromaAlignmentTest
+  };
 
   virtual bool __stdcall PlanarChromaAlignment(PlanarChromaAlignmentMode key) = 0;
 
@@ -1628,8 +1622,7 @@ public:
 }; // end class IScriptEnvironment_Avs25. Order is important.
 
 
-enum MtMode
-{
+enum MtMode {
   MT_INVALID = 0,
   MT_NICE_FILTER = 1,
   MT_MULTI_INSTANCE = 2,
